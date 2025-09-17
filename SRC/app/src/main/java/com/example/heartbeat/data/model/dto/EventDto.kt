f26ccc584b16769a0f553515ea2f899f7e6f9462
MhLogn <hamanhlong39@gmail.com>
2025-09-17 22:15:24 +0700

    chore: add event data layer

---
+++ SRC/app/src/main/java/com/example/heartbeat/data/model/dto/EventDto.kt
@@ -0,0 +1,16 @@
+package com.example.heartbeat.data.model.dto
+
+import com.example.heartbeat.domain.entity.user.Donor
+import com.google.firebase.Timestamp
+
+data class EventDto(
+    val name: String? = null,
+    val description: String? = null,
+    val location: String? = null,
+    val date: String? = null,
+    val time: String? = null,
+    val deadline: Timestamp? = null,
+    val donorList: List<Donor>? = null,
+    val capacity: Int? = null,
+    val donorCount: Int? = null
+)

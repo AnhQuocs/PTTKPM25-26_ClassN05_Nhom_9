f9e9b91e46273271a8f98155fac8e2e8f000439a
MhLogn <hamanhlong39@gmail.com>
2025-09-17 22:15:24 +0700

    chore: add event data layer

---
+++ SRC/app/src/main/java/com/example/heartbeat/data/model/mapper/EventMapper.kt
@@ -0,0 +1,50 @@
+package com.example.heartbeat.data.model.mapper
+
+import com.example.heartbeat.data.model.dto.EventDto
+import com.example.heartbeat.domain.entity.event.Event
+import com.google.firebase.Timestamp
+import kotlinx.datetime.Instant
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toInstant
+import kotlinx.datetime.toJavaInstant
+import kotlinx.datetime.toLocalDateTime
+import java.util.Date
+
+fun EventDto.toDomain(id: String): Event {
+    return Event(
+        id = id,
+        name = name.orEmpty(),
+        description = description.orEmpty(),
+        location = location.orEmpty(),
+        date = date.orEmpty(),
+        time = time.orEmpty(),
+        deadline = deadline?.toDate()
+            ?.toInstant()
+            ?.let { Instant.fromEpochMilliseconds(it.toEpochMilli()) }
+            ?.toLocalDateTime(TimeZone.currentSystemDefault())
+            ?: LocalDateTime(1970, 1, 1, 0, 0, 0),
+        donorList = donorList ?: emptyList(),
+        capacity = capacity ?: 0,
+        donorCount = donorCount ?: 0,
+    )
+}
+
+fun Event.toDto(): EventDto {
+    return EventDto(
+        name = name,
+        description = description,
+        location = location,
+        date = date,
+        time = time,
+        deadline = Timestamp(
+            Date.from(
+                deadline.toInstant(TimeZone.currentSystemDefault())
+                    .toJavaInstant()
+            )
+        ),
+        donorList = donorList,
+        capacity = capacity,
+        donorCount = donorCount
+    )
+}
--- SRC/app/src/main/java/com/example/heartbeat/domain/entity/event/Event.kt
+++ SRC/app/src/main/java/com/example/heartbeat/domain/entity/event/Event.kt
@@ -1,2 +1,17 @@
 package com.example.heartbeat.domain.entity.event
 
+import com.example.heartbeat.domain.entity.user.Donor
+import kotlinx.datetime.LocalDateTime
+
+data class Event(
+    val id: String,
+    val name: String,
+    val description: String,
+    val location: String,
+    val date: String,
+    val time: String,
+    val deadline: LocalDateTime,
+    val donorList: List<Donor>,
+    val capacity: Int,
+    val donorCount: Int
+)

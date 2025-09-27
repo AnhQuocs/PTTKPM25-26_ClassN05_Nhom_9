918549fbf5f1b5fbe73f1537e641fcb7c4cbc5d8
AnhQuocs <anhquocydkl@gmail.com>
2025-09-28 06:34:13 +0700

    feat(event): event card

---
+++ SRC/app/src/main/java/com/example/heartbeat/presentation/features/event/ui/EventCard.kt
@@ -0,0 +1,13 @@
+package com.example.heartbeat.presentation.features.event.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.heartbeat.domain.entity.event.Event
+
+@Composable
+fun EventCard(
+    modifier: Modifier = Modifier,
+    event: Event
+) {
+
+}
--- SRC/app/src/main/java/com/example/heartbeat/presentation/features/users/staff/ui/StaffScreen.kt
+++ SRC/app/src/main/java/com/example/heartbeat/presentation/features/users/staff/ui/StaffScreen.kt
@@ -1,20 +1,35 @@
 package com.example.heartbeat.presentation.features.users.staff.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.heartbeat.R
 import com.example.heartbeat.presentation.features.event.ui.EventForm
 import com.example.heartbeat.presentation.features.event.viewmodel.EventViewModel
 import com.example.heartbeat.presentation.features.hospital.viewmodel.HospitalViewModel
+import com.example.heartbeat.ui.dimens.AppShape
 import com.example.heartbeat.ui.dimens.Dimens
+import com.example.heartbeat.ui.theme.PinkLight
 
 @Composable
 fun StaffScreen(
@@ -29,22 +44,66 @@ fun StaffScreen(
         eventViewModel.getAllEvents()
     }
 
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-    ) {
+    Scaffold(
+        topBar = {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(Dimens.HeightXL2)
+                    .background(
+                        color = Color(0xFF2051E5),
+                        RoundedCornerShape(
+                            bottomStart = AppShape.SuperRoundedShape,
+                            bottomEnd = AppShape.SuperRoundedShape
+                        )
+                    )
+                    .clip(
+                        RoundedCornerShape(
+                            bottomStart = AppShape.SuperRoundedShape,
+                            bottomEnd = AppShape.SuperRoundedShape
+                        )
+                    )
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_bgr_top_bar),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .align(Alignment.TopStart)
+                )
+
+                Text(
+                    text = stringResource(id = R.string.events),
+                    style = MaterialTheme.typography.titleLarge,
+                    color = Color.White,
+                    modifier = Modifier
+                        .align(Alignment.BottomCenter)
+                        .padding(vertical = Dimens.PaddingM),
+                )
+            }
+        }
+    ) { paddingValues ->
         Column(
             modifier = Modifier
-                .fillMaxSize()
-                .padding(horizontal = Dimens.PaddingM),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text("Staff Screen")
-
-            EventForm(
-                list = hospitals
-            )
-        }
+                .padding(paddingValues)
+        ) { }
     }
+
+//    Box(
+//        modifier = Modifier
+//            .fillMaxSize()
+//    ) {
+//        Column(
+//            modifier = Modifier
+//                .fillMaxSize()
+//                .padding(horizontal = Dimens.PaddingM),
+//            verticalArrangement = Arrangement.Center,
+//            horizontalAlignment = Alignment.CenterHorizontally
+//        ) {
+//            Text("Staff Screen")
+//
+//            EventForm(
+//                list = hospitals
+//            )
+//        }
+//    }
 }
